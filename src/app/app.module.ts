a16242dbe0126aa099860c3cb45e4e26a23d7b86
dbsaymen <dbs.aymen@gmail.com>
2018-07-12 12:44:32 +0200

    fix importation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { NavbarrComponent}  from "./shared/navbarr/navbarr.component";
+import { FooterComponent}  from "./shared/footer/footer.component";
+import { TestComponent } from './test/test.component'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarrComponent,
+    FooterComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule
