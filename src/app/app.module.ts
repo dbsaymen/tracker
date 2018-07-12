2f89e64b55aa2da6b6512ad170e553d970b7902f
dbsaymen <dbs.aymen@gmail.com>
2018-07-12 15:20:01 +0200

    add nav-barr footer social-media

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavbarrComponent}  from "./shared/navbarr/navbarr.component";
 import { FooterComponent}  from "./shared/footer/footer.component";
-import { TestComponent } from './test/test.component'
+import { TestComponent } from './test/test.component';
+import { SocialMediaComponent } from './shared/social-media/social-media.component'
 @NgModule({
   declarations: [
     AppComponent,
     NavbarrComponent,
     FooterComponent,
-    TestComponent
+    TestComponent,
+    SocialMediaComponent
   ],
   imports: [
     BrowserModule
